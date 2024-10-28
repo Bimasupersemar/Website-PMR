90ebf5ea1e1a50291f4ca818df343dd80dc77740
Christina <teresiaflorencia@gmail.com>
2024-10-28 09:52:51 +0700

    Update halaman_input.php

--- admin/halaman_input.php
+++ admin/halaman_input.php
@@ -1,5 +1,6 @@
-<?php include("inc_header.php")?>
-<?php 
+<?php include("inc_header.php"); // Pastikan ini mendefinisikan variabel $koneksi
+?>
+<?php
 $judul = "";
 $isi = "";
 $kutipan = "";
@@ -14,14 +15,17 @@ if (isset($_POST['simpan'])) {
     if ($judul == '' or $isi == '') {
         $error = "silahkan masukan semua data";
     }
-   
+
     if (empty($error)) {
-        $sql1 = "insert into halaman(judul,kutipan,isi) values ('$judul','$kutipan','$isi')";
-        $q1 = mysqli_query($koneksi,$sql1);
-        if ($q1){
+        $sql1 = "INSERT INTO `halaman-web` (judul, kutipan, isi) VALUES ('$judul', '$kutipan', '$isi')";
+        $q1 = mysqli_query($koneksi, $sql1);
+
+        $hasil = mysqli_query($koneksi, $sql1);
+
+        if ($q1) {
             $sukses = "Berhasil memasukkan data";
-         } else {
-            $error = "Gagal memasukkan data"; 
+        } else {
+            $error = "Gagal memasukkan data: " . mysqli_error($koneksi);
         }
     }
 }
@@ -32,58 +36,48 @@ if (isset($_POST['simpan'])) {
 <div class="mb-3 row">
     <a href="halaman.php">Kembali ke Halaman Admin</a>
 </div>
-<?php
-if ($error)
-{
-    ?>
-    
+<?php if ($error) { ?>
     <div class="alert alert-danger" role="alert">
-  <?php echo $error ?>
-</div>
-<?php
-}
-?>
-<?php
-if ($sukses)
-{
-    ?>
-    
-    <div class="alert alert-primary" role="alert">
-  <?php echo $sukses ?>
-</div>
-<?php
-}
-?>
-<form action="" method="post">
-<div class="mb-3 row">
-    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
-    <div class="col-sm-10">
-    <label for="judul" class="visually-hidden">Judul</label>
-    <input type="text" class="form-control" id="judul" value="<?php echo $judul ?>" name="judul">
-  </div>
-  </div>
-<div class="mb-3 row">
-    <label for="kutipan" class="col-sm-2 col-form-label">kutipan</label>
-    <div class="col-sm-10">
-    <label for="kutipan" class="visually-hidden">Kutipan</label>
-    <input type="text" class="form-control" id="kutipan" value="<?php echo $kutipan ?>" name="kutipan">
-  </div>
-  </div>
-<div class="mb-3 row">
-    <label for="isi" class="col-sm-2 col-form-label">Isi</label>
-    <div class="col-sm-10">
-    <label for="isi" class="visually-hidden">Isi</label>
-  <textarea name="isi" class="form-control" id="summernote"><?php echo $isi ?></textarea>
-  </div>
-  </div>
-  <div class="mb-3 row">
-    <div class="col-sm-10">
-    <div class="col-sm-2">
-   <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary">
-  </div>
-  </div>
+        <?php echo $error ?>
+    </div>
+<?php } ?>
 
-  
+<?php if ($sukses) { ?>
+    <div class="alert alert-primary" role="alert">
+        <?php echo $sukses ?>
+    </div>
+<?php } ?>
+
+<form action="" method="post">
+    <div class="mb-3 row">
+        <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+        <div class="col-sm-10">
+            <label for="judul" class="visually-hidden">Judul</label>
+            <input type="text" class="form-control" id="judul" value="<?php echo $judul ?>" name="judul">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="kutipan" class="col-sm-2 col-form-label">kutipan</label>
+        <div class="col-sm-10">
+            <label for="kutipan" class="visually-hidden">Kutipan</label>
+            <input type="text" class="form-control" id="kutipan" value="<?php echo $kutipan ?>" name="kutipan">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label for="isi" class="col-sm-2 col-form-label">Isi</label>
+        <div class="col-sm-10">
+            <label for="isi" class="visually-hidden">Isi</label>
+            <textarea name="isi" class="form-control" id="summernote"><?php echo $isi ?></textarea>
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <div class="col-sm-10">
+            <div class="col-sm-2">
+                <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary">
+            </div>
+        </div>
+
+
 </form>
 </form>
-<?php include("inc_footer.php")?>
+<?php include("inc_footer.php") ?>
