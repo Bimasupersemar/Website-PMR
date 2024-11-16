d6a83428b69cdd0f05190850d1f0f5768ee91990
Christina <teresiaflorencia@gmail.com>
2024-11-16 14:16:16 +0700

    Update pendaftaran.php

--- pendaftaran.php
+++ pendaftaran.php
@@ -7,60 +7,23 @@ if(isset($_SESSION['members_email']) != ''){ //sudah dalam keadaan login
 ?>
 <h3>Pendaftaran</h3>
 <?php 
-$email          = "";
 $nama_lengkap   = "";
-$err            = "";
-$sukses         = "";
+$kelas            = "";
+$nis            = "";
+$gender         = "";
+$alamat         = "";
+$nomor        = "";
 
 if(isset($_POST['simpan'])){
-    $email                  = $_POST['email'];
     $nama_lengkap           = $_POST['nama_lengkap'];
-    $password               = $_POST['password'];
-    $konfirmasi_password    = $_POST['konfirmasi_password'];
+    $kelas               = $_POST['kelas'];
+    $nis               = $_POST['nis'];
+    $gender    = $_POST['gender'];
+    $alamat   = $_POST['alamat'];
+    $nomor    = $_POST['nomor'];
 
-    if($email == '' or $nama_lengkap == '' or $konfirmasi_password =='' or $password == ''){
+    if($nama_lengkap == '' or $kelas == '' or $nis =='' or $gender == '' or $alamat == '' or $nomor == ''){
         $err .= "<li>Silakan masukkan semua isian.</li>";
-    }
-
-    //cek di bagian db, apakah email sudah ada atau belum
-    if($email != ''){
-        $sql1   = "select email from members where email = '$email'";
-        $q1     = mysqli_query($koneksi,$sql1);
-        $n1     = mysqli_num_rows($q1);
-        if($n1 > 0){
-            $err .= "<li>Email yang kamu masukkan sudah terdaftar.</li>";
-        }
-
-        //validasi email
-        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-            $err .= "<li>Email yang kamu masukkan tidak valid.</li>";
-        }
-    }
-
-    //cek kesesuaian password & konfirmasi password
-    if($password != $konfirmasi_password){
-        $err .= "<li>Password dan Konfirmasi Password tidak sesuai.</li>";
-    }
-    if(strlen($password) < 6){
-        $err .= "<li>Panjang karakter yang diizinkan untuk password paling tidak 6 karakter.</li>";
-    }
-
-    if(empty($err)){
-        $status             = md5(rand(0,1000));
-        $judul_email        = "Halaman Konfirmasi Pendaftaran";
-        $isi_email          = "Akun yang kamu miliki dengan email <b>$email</b> telah siap digunakan.<br/>";
-        $isi_email          .= "Sebelumnya silakan melakukan aktifasi email di link di bawah ini:<br/>";
-        $isi_email          .= url_dasar()."/verifikasi.php?email=$email&kode=$status";
-
-        kirim_email($email,$nama_lengkap,$judul_email,$isi_email);
-
-        $sql1       = "insert into members(email,nama_lengkap,password,status) values ('$email','$nama_lengkap',md5($password),'$status')";
-        $q1         = mysqli_query($koneksi,$sql1);
-        if($q1){
-            $sukses = "Proses Berhasil. Silakan cek email kamu untuk verifikasi.";
-        }
-
-        
     }
 
 }
@@ -71,31 +34,24 @@ if(isset($_POST['simpan'])){
 <form action="" method="POST">
     <table>
         <tr>
-            <td class="label">Email</td>
-            <td>
-                <input type="text" name="email" class="input" value="<?php echo $email?>"/>
-            </td>
-        </tr>
-        <tr>
             <td class="label">Nama Lengkap</td>
             <td>
                 <input type="text" name="nama_lengkap" class="input" value="<?php echo $nama_lengkap?>"/>
             </td>
         </tr>
         <tr>
-            <td class="label">Password</td>
+            <td class="label">Kelas</td>
             <td>
-                <input type="password" name="password" class="input" />
+                <input type="text" name="kelas" class="input" value="<?php echo $kelas?>"/>
             </td>
         </tr>
         <tr>
-            <td class="label">Konfirmasi Password</td>
+            <td class="label">NIS</td>
             <td>
-                <input type="password" name="konfirmasi_password" class="input" />
-                <br/>
-                Sudah punya akun? Silakan <a href='<?php echo url_dasar()?>/login.php'>login</a>
+                <input type="number" name="nis" class="input" value="<?php echo $nis?>" />
             </td>
         </tr>
+        <select name="gender" class="input" value=""></select>
         <tr>
             <td></td>
             <td>
